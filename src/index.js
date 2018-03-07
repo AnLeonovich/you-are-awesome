513b14b939687efd78c0d45ec9dad14a632ecdd9
AnLeonovich <novka666@mail.ru>
2018-03-07 22:41:28 +0300

    Check 5/10

--- src/index.js
+++ src/index.js
@@ -4,7 +4,31 @@ const createEnumerableProperty = (propertyName) => {
     return propertyName;
 };
 
+const createNotEnumerableProperty = (propertyName) => {
+    return Symbol(propertyName);
+};
 
+const createProtoMagicObject = () => {
+	var magicObject = function(){};
+    magicObject.prototype = magicObject.__proto__;
+    return magicObject;
+};
+
+var counter = 0;
+const incrementor = () => {
+	incrementor.toString = function(){
+        return counter;
+    };
+    counter++;
+    return incrementor;
+};
+
+counter = 0;
+const asyncIncrementor = () => {
+    return new Promise((resolve) => {
+	resolve (counter++);	
+    });
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
